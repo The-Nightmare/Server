c924e5f378871c638cd94da0c90351bd57752845
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-08-23 22:03:03 -0600

    lowercase commits fer dayz

---
+++ auth/index.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+var queries = require('../queries/apiQueries');
+var auth = require('./helpers.js');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+
+router.post('/signup', function(req, res, next){
+  queries.findUserByUserName(req.query.userName)
+  .then(function(user){
+    if(user){
+      res.json({
+        error : "user already exist try another name"
+      });
+    }else {
+      auth.createUser(req.query)
+      .then(function(user){
+        res.json({
+          message : 'you are a new user yay'
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', function(req, res, next){
+  queries.findUserByUserName(req.query.userName)
+  .then(function(user){
+    var plainTextPassword = req.query.password;
+    if(user && bcrypt.compareSync(plainTextPassword, user.password)){
+      jwt.sign(user, process.env.TOKEN_SECRET, {expiresIn: '1d'}, function(err, token){
+        if(err){
+          res.json({
+            message: 'error creating token'
+          });
+        }else{
+          res.json({
+            token : token,
+            userId: user.id
+          });
+        }
+      });
+      }else{
+      res.status(401);
+      res.json({
+        message : 'unauthorized'
+      });
+    }
+  });
+});
+
+module.exports = router;
